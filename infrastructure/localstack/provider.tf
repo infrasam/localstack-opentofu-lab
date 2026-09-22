b9314643a6c369f3ae2e068b8346a9185536e060
Samuel Gustavsson <samuel.gustavsson93@hotmail.com>
2026-09-22 13:14:42 +0200

    deploy initial s3-bucket

---
+++ infrastructure/localstack/provider.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = "test"
+  secret_key = "test"
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+
+  s3_use_path_style = true
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    sts      = "http://localhost:4566"
+  }
+}

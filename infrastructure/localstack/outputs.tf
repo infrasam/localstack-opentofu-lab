27381b49abc6b49bfb89be7aa38efd711b62106a
Samuel Gustavsson <samuel.gustavsson93@hotmail.com>
2026-09-23 13:11:08 +0200

    feat: add public subnet to VPC

--- infrastructure/localstack/outputs.tf
+++ infrastructure/localstack/outputs.tf
@@ -2,3 +2,13 @@ output "bucket_name" {
   description = "The name of the S3 bucket."
   value       = module.bucket.bucket_name
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet."
+  value       = module.network.public_subnet_id
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,22 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = true
 
   tags = {
+
     Name        = var.name
+    Project     = "localstack-opentofu-lab"
+    Environment = var.environment
+    ManagedBy   = "opentofu"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.subnet_cidr_block
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.name}-public-subnet"
     Project     = "localstack-opentofu-lab"
     Environment = var.environment
     ManagedBy   = "opentofu"
